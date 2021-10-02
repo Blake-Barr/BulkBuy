6f4e400594e421da84d3bb646b7151860472fcf7
Brandon Matalka <matalkbt@mail.uc.edu>
2021-10-01 22:23:11 -0400

    Init

---
+++ src/test/java/com/bulkbuy/enterprise/EnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulkbuy.enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
