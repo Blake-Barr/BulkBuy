7f518f25c299e7937e3b27dc62ce768e2af90b7a
Blakedasnake727 <barrblake555@gmail.com>
2021-11-01 19:50:29 -0400

    Added documentation for OrderRepository and OrderDAO

--- src/main/java/com/bulkbuy/enterprise/dao/OrderDAO.java
+++ src/main/java/com/bulkbuy/enterprise/dao/OrderDAO.java
@@ -7,17 +7,29 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
+/***
+ * Handles business logic for order Data
+ */
 @Repository
 public class OrderDAO implements IOrderDAO {
 
     OrderRepository orderRepository;
 
+    /***
+     * Creates an order to put into the database
+     * @param order the order to create
+     * @return the created order
+     */
     @Override
     public Order create(Order order) {
         Order created = orderRepository.save(order);
         return created;
     }
 
+    /***
+     * Gets all orders in the database
+     * @return a list containing all orders
+     */
     @Override
     public List<Order> getAllOrders()   {
         List<Order> allOrders = new ArrayList<>();
@@ -29,6 +41,11 @@ public class OrderDAO implements IOrderDAO {
         return allOrders;
     }
 
+    /***
+     * Finds an order in the database by the given id
+     * @param id the id to lookup
+     * @return the order with the specified id
+     */
     @Override
     public Order findByOrderId(int id)    {
         return orderRepository.findById(id).get();
--- src/main/java/com/bulkbuy/enterprise/dao/OrderRepository.java
+++ src/main/java/com/bulkbuy/enterprise/dao/OrderRepository.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/***
+ * Crud Repository responsible for providing various methods to interact with the database
+ */
 @Profile("TBA")
 public interface OrderRepository extends CrudRepository<Order, Integer> {
 
