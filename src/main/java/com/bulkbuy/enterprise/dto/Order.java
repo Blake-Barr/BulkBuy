09b72d80f994d72fd15966c8b80df57d63e3678a
Blakedasnake727 <barrblake555@gmail.com>
2021-11-14 12:32:24 -0500

    Set up database authentication, added compatibility for database storage to Order

--- src/main/java/com/bulkbuy/enterprise/dto/Order.java
+++ src/main/java/com/bulkbuy/enterprise/dto/Order.java
@@ -3,11 +3,19 @@ package com.bulkbuy.enterprise.dto;
 import lombok.Data;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * DTO, handles data for orders.
  */
+@Entity
 public @Data class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int orderId;
     private int quantity;
     private String productDescription;
