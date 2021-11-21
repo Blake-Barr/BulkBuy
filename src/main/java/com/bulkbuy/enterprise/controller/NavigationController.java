7757b8fe2e4539700481d1f7e699699a541e56e6
Brandon Matalka <matalkbt@mail.uc.edu>
2021-11-21 15:06:11 -0500

    Added logging to Controller and added log configuration file for spring

--- src/main/java/com/bulkbuy/enterprise/controller/NavigationController.java
+++ src/main/java/com/bulkbuy/enterprise/controller/NavigationController.java
@@ -1,6 +1,8 @@
 package com.bulkbuy.enterprise.controller;
 import com.bulkbuy.enterprise.dto.Order;
 import com.bulkbuy.enterprise.service.IOrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class NavigationController {
 
+    Logger log = LoggerFactory.getLogger((this.getClass()));
     IOrderService orderService;
 
     /**
@@ -17,18 +20,21 @@ public class NavigationController {
 
     @GetMapping("/index")
     public String getIndex(Model model){
+        log.debug("Index endpoint reached");
         model.addAttribute("activePage", "index");
         return "index";
     }
 
     @GetMapping("/about")
     public String getAbout(Model model){
+        log.debug("About endpoint reached");
         model.addAttribute("activePage", "about");
         return "about";
     }
 
     @GetMapping("/placeOrder")
     public String getPlaceOrder(Model model){
+        log.debug("Place order endpoint reached");
         Order order = new Order();
         model.addAttribute("activePage", "placeOrder");
         model.addAttribute(order);
@@ -37,19 +43,22 @@ public class NavigationController {
 
     @GetMapping("/orderLookup")
     public String getOrderLookup(Model model){
+        log.debug("Order lookup endpoint reached");
         model.addAttribute("activePage", "orderLookup");
         return "orderLookup";
     }
 
     @PostMapping(value="/saveOrder", consumes = "application/json", produces = "application/json")
     public Order saveOrder(@RequestBody Order order) throws Exception {
+        log.debug("Save order endpoint reached");
         Order newOrder = null;
         try {
             newOrder = orderService.create(order);
+            log.info("New order created successfully");
         }
         catch (Exception ex)
         {
-            //TODO: LOGGING/Exception handling
+            log.error("failed to save order", ex);
             throw ex;
         }
 
